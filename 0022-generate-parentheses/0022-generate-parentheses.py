a4c19067335e832fbe040cb723972e78740050b2
Sandeep Reddy Yeruva <79009504+Sandeep3899@users.noreply.github.com>
2025-09-10 16:05:12 -0700

    Time: 0 ms (100%), Space: 18.1 MB (25.46%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -2,21 +2,21 @@ from typing import List
 
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        res: List[str] = []
+        res = []
+        stack = []
 
-        def backtrack(path: str, open_cnt: int, close_cnt: int) -> None:
-            # If the string is complete, add to results
-            if len(path) == 2 * n:
-                res.append(path)
+        def backtrack(openN, closeN):
+
+            if (openN == closeN == n):
+                res.append("".join(stack))
                 return
-
-            # Try to place an opening parenthesis if we still can
-            if open_cnt < n:
-                backtrack(path + "(", open_cnt + 1, close_cnt)
-
-            # Place a closing parenthesis only if it won't invalidate the string
-            if close_cnt < open_cnt:
-                backtrack(path + ")", open_cnt, close_cnt + 1)
-
-        backtrack("", 0, 0)
+            if openN < n:
+                stack.append("(")
+                backtrack(openN+1,closeN)
+                stack.pop()
+            if closeN<openN:
+                stack.append(")")
+                backtrack(openN,closeN+1)
+                stack.pop()
+        backtrack(0,0)
         return res
