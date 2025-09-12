e452e6180c4e72d6373ce11986cee4aa1400a751
Sandeep Reddy Yeruva <79009504+Sandeep3899@users.noreply.github.com>
2025-09-12 15:56:58 -0700

    Time: 53 ms (25.38%), Space: 19.9 MB (29.3%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -1,9 +1,15 @@
+class ListNode:
+    def __init__(self, val = 0, next = None):
+        self.val = val
+        self.next = next
+
 class Solution:
-    def hasCycle(self, head: Optional[ListNode]) -> bool:
-        visited = set()
-        while head:
-            if head in visited:
+    def hasCycle(self, head) -> bool:
+        s, f = head, head
+
+        while f and f.next:
+            s = s.next
+            f = f.next.next
+            if s == f:
                 return True
-            visited.add(head)
-            head = head.next
         return False
